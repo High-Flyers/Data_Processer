26dca31d378b6c3ee071b657934229ef2d171b95
InKronos <i15.piorkowski@gmail.com>
2020-05-10 00:36:20 +0200

    creating pdf from html + pandas beginning

---
+++ mapploter/mapploter.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import json
+
+
+with open('log_ksztalty.json') as json_data:
+    dataset  = json.loads(json_data.read())
+    json_data.close()
+    
+#print(dataset['Figures'])
+#for data in dataset['Figures']:
+    #print(data['Coords'])
+
+coords = pd.json_normalize(dataset['Figures'])
+print(coords['Coords'])
+print('as')
+BBox = ((18.9351,   19.0189,      
+         51.9662, 52.0127))
+#df = pd.read_csv('coords.csv')         
+df = pd.DataFrame([[52.0001, 18.9995, 0]],
+                  columns=['length', 'width', 'species'])
+ruh_m = plt.imread('map.png')
+fig, ax = plt.subplots(figsize = (8, 7))
+ax = df.plot.scatter(df['lenght'], df['width'], c='DarkBlue', s=10)
+ax.set_title('plottong')
+ax.set_xlim(BBox[0], BBox[1])
+ax.set_ylim(BBox[2], BBox[3])
+print('as')
+ax.imshow(ruh_m, zorder=0, extent = BBox, aspect= 'equal')
+plt.show()
+print('as')
