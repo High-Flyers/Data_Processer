c81ece88b091e819b8d726664f4e27ac1e1ef519
InKronos <i15.piorkowski@gmail.com>
2020-07-08 22:44:10 +0200

    commenting templater and writing README.md

--- README.md
+++ README.md
@@ -1,28 +1,36 @@
-#
-#
-#
-#Processing data from json and presenting it to human form
-#
-#
-#Remember to have pdfkit
-#And
-#sudo apt-get install wkhtmltopdf
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+# Data Processer 
+
+Taking data from json file and changing it to PDF file
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install additional modules.
+
+```bash
+pip install pandas
+pip install numpy
+pip install matplotlib
+pip install pdfkit
+pip install argparse
+```
+
+## Usage
+
+```bash
+python mapploter.py -j file.json
+```
+
+Remember to change image paths
+```HTML
+<link rel="stylesheet" href="style.css">
+<img alt="logo_hf" src="pictures/hf_logo.png" id="logo_png">
+```
+
+## Results 
+Here you have sample of result:
+
+![image of the sample](preview/sample.png)
+
+
+## Author
+Code was made by **InKronos**
--- mapploter/htmltemplater.py
+++ mapploter/htmltemplater.py
@@ -4,18 +4,63 @@ import os
 
 
 class HTMLTemplater:
-    
+    """A class used to change dynamically HTML code
+
+     Attributes
+    ----------
+    filepath : str
+        path to html file
+
+    Methods
+    -------
+    replace(fillMap)
+        Replacing HTML commented code intro values in parametr
+
+    replaceImage(key, filepath)
+        Replacing HTML commented code intro image path
+
+    save(outpath)
+        Saving HTML code
+    """
+
     def __init__(self, filepath):
+        """
+        Parameters
+        ----------
+        filepath : str
+            path to file
+
+        """
+        
         self.template = None
         with open(filepath, 'r') as file:
             self.template = file.read()
         
     
     def replace(self, fillMap):
+        """Replacing HTML commented code intro values in parametr
+
+        Parameters
+        ----------
+        fillMap : array
+            array with values to replace
+
+        """
+
         for (key, value) in fillMap.items():
             self.template = self.template.replace(f'<!-- model-replace: {key} -->', str(value))
 
     def replaceImage(self, key, filepath):
+        """Replacing HTML commented code intro image path
+
+        Parameters
+        ----------
+        key : str
+            string with identification which comment to change
+        filepath : str
+            path to image
+        """
+
         with open(filepath, 'rb') as image_file:
             encoded_string = base64.b64encode(image_file.read())
             encoded_string = 'data:image/jpeg;base64,' + encoded_string.decode('utf-8')
@@ -23,6 +68,14 @@ class HTMLTemplater:
             self.template = self.template.replace(f'<!-- model-replace: {key} -->', encoded_string)
     
     def save(self, outpath):
+        """Saving HTML code
+        
+        Parameters
+        ----------
+        outpath : str
+            string with output patch
+        """
+
         options={
              'page-size': 'Letter',
             'margin-top': '0.0in',
